cce3db547f1f9dd34a6cd5faffff32660c1bef53
Bhushan Gawande <brgawande@gmail.com>
2024-10-14 01:06:39 +0530

    login user functionalityy completed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,6 @@
 const User = require("../model/userScehma");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = async (req, res) => {
   const { name, email, password, phone } = req.body;
@@ -37,6 +38,60 @@ exports.registerUser = async (req, res) => {
   } catch (error) {
     res.status(404).json({
       sucess: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(404).json({
+      success: false,
+      message: "Email And Password are Required",
+    });
+  }
+  try {
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      return res.status(200).json({
+        success: false,
+        message: "Email Dosen't Exist",
+      });
+    }
+
+    const passwordMatched = await bcrypt.compare(password, userExist?.password);
+
+    if (!passwordMatched) {
+      return res.status(404).json({
+        success: false,
+        message: "Password Dosen't Matched",
+      });
+    }
+
+    //   generate jwt token
+    const token = jwt.sign(
+      { id: userExist._id, email: userExist.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: `Welcome ${userExist?.name}`,
+      token,
+      user: {
+        id: userExist._id,
+        name: userExist.name,
+        email: userExist.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
       message: error.message,
     });
   }
